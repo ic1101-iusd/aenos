18140abb2acd6a831654321ebff4280655fb3a83
Norfolks <norfolknorfolks@gmail.com>
2022-06-08 19:18:06 +0300

    Contract dev (#8)
    * Start to implement Minter
    
    * Now it compiles
    
    * Add collateral actor; Make createPostion func work as it should.
    
    * Add query method for postion
    
    * Make usb token init function and integrate to protocol
    
    * Add declarations; Remove mint_token dependency in protocol.
    
    * Add result to sume func; Add close position func.
    
    * Add liquidation method

--- src/declarations/protocol/protocol.did.d.ts
+++ src/declarations/protocol/protocol.did.d.ts
@@ -1,12 +1,17 @@
 import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 
+export type ProtocolError = { 'transferFromError' : null };
+export type Result = { 'ok' : null } |
+  { 'err' : ProtocolError };
 export type SharedPosition = {};
 export interface _SERVICE {
-  'createPosition' : ActorMethod<[bigint, bigint], undefined>,
+  'closePosition' : ActorMethod<[bigint], Result>,
+  'createPosition' : ActorMethod<[bigint, bigint], Result>,
   'getCollateralPrice' : ActorMethod<[], bigint>,
   'getPosition' : ActorMethod<[bigint], [] | [SharedPosition]>,
   'getTokenPrincipal' : ActorMethod<[], Principal>,
   'init' : ActorMethod<[string, string], undefined>,
+  'liquidatePosition' : ActorMethod<[bigint], Result>,
   'setCollateralPrice' : ActorMethod<[bigint], undefined>,
 }
--- src/declarations/protocol/protocol.did.js
+++ src/declarations/protocol/protocol.did.js
@@ -1,11 +1,15 @@
 export const idlFactory = ({ IDL }) => {
+  const ProtocolError = IDL.Variant({ 'transferFromError' : IDL.Null });
+  const Result = IDL.Variant({ 'ok' : IDL.Null, 'err' : ProtocolError });
   const SharedPosition = IDL.Record({});
   return IDL.Service({
-    'createPosition' : IDL.Func([IDL.Nat, IDL.Nat], [], []),
+    'closePosition' : IDL.Func([IDL.Nat], [Result], []),
+    'createPosition' : IDL.Func([IDL.Nat, IDL.Nat], [Result], []),
     'getCollateralPrice' : IDL.Func([], [IDL.Nat], ['query']),
     'getPosition' : IDL.Func([IDL.Nat], [IDL.Opt(SharedPosition)], ['query']),
     'getTokenPrincipal' : IDL.Func([], [IDL.Principal], ['query']),
     'init' : IDL.Func([IDL.Text, IDL.Text], [], []),
+    'liquidatePosition' : IDL.Func([IDL.Nat], [Result], []),
     'setCollateralPrice' : IDL.Func([IDL.Nat], [], []),
   });
 };
